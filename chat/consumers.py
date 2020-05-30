7df25f926603daccdef3f79da0ebda98bfb91574
gabriel <jeronimogbariel@gmail.com>
2020-05-30 16:45:23 -0300

    fatorando código

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,11 +4,16 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.room_group_name = None
+        self.room_name = None
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        print(self.channel_name)
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
